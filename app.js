5b689ebe9b6d78320b1c6a9f3d56387f381c9c16
Gustavo Rohrer <gustavorohrer@gmail.com>
2020-10-03 11:07:16 -0300

    Add errorHandlers middlewares

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const errorHandlers = require("./handlers/errorHandlers");
 
 const indexRouter = require("./routes/index");
 
@@ -14,5 +15,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+
+app.use(errorHandlers.notFound);
+
+if (app.get("env") === "development") {
+  /* Development Error Handler - Prints stack trace */
+  app.use(errorHandlers.developmentErrors);
+}
+
+// production error handler
+app.use(errorHandlers.productionErrors);
 
 module.exports = app;
