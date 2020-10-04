9c2cc5aa3a57e6aa0be6508a12645b3a8fab8dc9
Gustavo Rohrer <gustavorohrer@gmail.com>
2020-10-04 15:24:40 -0300

    Update readme.md, Finish buildCard function

--- readme.md
+++ readme.md
@@ -2,6 +2,7 @@
 
 - With a Trello account, get YOUR_TRELLO_KEY, YOUR_TRELLO_TOKEN (https://trello.com/app-key)
 - It is assumed that a Trello board already exists, and it has a "To Do" list (it needs to be called like that)
+- It is also assumed that the followings labels exist on the Trello board: "Bug", "Maintenance", "Research" and "Test"
 - Retrieve the board id with `curl 'https://api.trello.com/1/members/me/boards?key={YOUR_TRELLO_KEY}&token={YOUR_TRELLO_TOKEN}'` (the `idBoard` attribute of the response)
 - In the project, rename the `variables.env.sample` to `variables.env` and complete Trello variables
 - `mpm install`
--- services/taskManagerService.js
+++ services/taskManagerService.js
@@ -20,32 +20,46 @@ const randomizeTitle = async (desc, listId) => {
   return `bug-${randomWord}-${bugNumber}`;
 };
 
-function getIdLabelByCategory() {
-  //TODO
-  return undefined;
-}
+const getIdLabelByCategory = async (category) => {
+  const labels = await taskManagerClient.getLabelsForBoard(
+    process.env.TRELLO_BOARD_ID
+  );
+  return [labels.find((label) => label.name === category).id];
+};
+
+const getRandomMemberId = async () => {
+  const membersIds = await taskManagerClient.getBoardMembers(
+    process.env.TRELLO_BOARD_ID
+  );
+  return [membersIds[Math.floor(Math.random() * membersIds.length)].id];
+};
+
+const getBugLabel = async () => {
+  const labels = await taskManagerClient.getLabelsForBoard(
+    process.env.TRELLO_BOARD_ID
+  );
+  return [labels.find((label) => label.name === "Bug").id];
+};
 
 const buildCard = async ({ type, title, description, category }) => {
   let name = title;
-
   const extraParams = {
     desc: description,
   };
-
   const listId = await getListId();
 
   //FIXME refactor to scale if new flavors are added
   switch (type) {
     case "bug":
       name = await randomizeTitle(description, listId);
-      extraParams.idMembers = []; //TODO refactor this
-      extraParams.idLabels = []; //TODO refactor this
+      extraParams.idMembers = await getRandomMemberId();
+      extraParams.idLabels = await getBugLabel();
       break;
     case "issue":
-      //TODO check if exists?
+      //TODO check if exists? (It was not specified)
       break;
     case "task":
-      extraParams.idLabels = getIdLabelByCategory(category);
+      extraParams.idLabels = await getIdLabelByCategory(category);
       break;
     default:
       throw Error("Invalid card type!");
